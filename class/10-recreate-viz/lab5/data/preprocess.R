f31e90bbb531bcb7dcf1d5e9df5250b7366a9f1b
Xindi Hu <xindyhu@users.noreply.github.com>
2026-06-17 16:32:44 -0400

    feat: update class 11 details

--- class/10-recreate-viz/lab5/data/preprocess.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-
-ucmr5 <- read_csv("data/UCMR5_data_allsamples.csv")
-
-dat <- ucmr5 %>%
-  dplyr::select(PWSID, PWSName, State, CollectionDate, FacilityName, Size)
-
-write_csv(dat, "data/ucmr5_dat.csv")
